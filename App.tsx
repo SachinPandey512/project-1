1e49faf59562c3409b9ff06c87ae4a9b7191cf21
SachinPandey512 <2003512sachin@gmail.com>
2024-12-09 16:57:04 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import HeroSection from "../hero";
+import Navigation from "../navigation";
+const App = () => {
+    return <div> 
+     <Navigation/>
+     <HeroSection/>
+   </div>
+};
+ export default App;
